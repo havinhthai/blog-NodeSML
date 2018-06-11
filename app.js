dc5864248811620659fb908cd021a64f455e861b
Vinh Thai <vinhthai997@gmail.com>
2018-06-11 13:38:32 +0700

    Connect mlab

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const { port } = require('./config/config'); // { port: 3000 }
+const { port, dbUrl } = require('./config/config'); // { port: 3000 }
 const routers = require('./routers');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(dbUrl);
+mongoose.connection.on('error', (err) => {
+    console.log('Lỗi kết nối đến CSDL: ' + err);
+});
 
 const app = express();
 
--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     port: 3000,
+    dbUrl: 'mongodb://root:123456a@ds155730.mlab.com:55730/sgr-blog',
 };
